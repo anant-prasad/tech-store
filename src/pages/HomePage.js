3cac0eff703a4d4b0aa33d0bfd4f8796d95ea7f8
Anant Prasad <anant29october@gmail.com>
2020-06-08 18:08:21 +0530

    fixed bugs from previous components & main-link in hero component added

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,15 @@
-import React from 'react';
- 
-import Hero from '../components/Hero'
-import { Link } from 'react-router-dom'
+import React from "react";
+import Hero from "../components/Hero";
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
-    return (
-        <>
- <Hero title="awesome gadgets" max="true">
-     <Link to="/product">
-         Our Products
-     </Link>
- </Hero>
+  return (
+    <>
+      <Hero title="awesome gadgets" max="true">
+        <Link to="/products" className="main-link" style={{ margin: "2rem" }}>
+          our products
+        </Link>
+      </Hero>
     </>
-    )
+  );
 }
